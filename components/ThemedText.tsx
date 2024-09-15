7f41b93858e225366e90e56d045bf6224bb0485a
Baffuoh Asare-Bediako <104689768+baffuohasareb@users.noreply.github.com>
2024-09-15 22:46:09 +0000

    Updated text variations

--- components/ThemedText.tsx
+++ components/ThemedText.tsx
@@ -3,34 +3,34 @@ import { Text, type TextProps, StyleSheet } from 'react-native';
 import { useThemeColor } from '@/hooks/useThemeColor';
 
 export type ThemedTextProps = TextProps & {
-  lightColor?: string;
-  darkColor?: string;
-  type?: 'default' | 'title' | 'defaultSemiBold' | 'subtitle' | 'link';
+    lightColor?: string;
+    darkColor?: string;
+    type?: 'default' | 'heading' | 'cardTitle' | 'cardText' | 'small';
 };
 
 export function ThemedText({
-  style,
-  lightColor,
-  darkColor,
-  type = 'default',
-  ...rest
+    style,
+    lightColor,
+    darkColor,
+    type = 'default',
+    ...rest
 }: ThemedTextProps) {
-  const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
+    const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
 
-  return (
-    <Text
-      style={[
-        { color },
-        type === 'default' ? styles.default : undefined,
-        type === 'title' ? styles.title : undefined,
-        type === 'defaultSemiBold' ? styles.defaultSemiBold : undefined,
-        type === 'subtitle' ? styles.subtitle : undefined,
-        type === 'link' ? styles.link : undefined,
-        style,
-      ]}
-      {...rest}
-    />
-  );
+    return (
+        <Text
+			style={[
+				{ color },
+				type === 'default' ? styles.default : undefined,
+				type === 'heading' ? styles.heading : undefined,
+				type === 'cardTitle' ? styles.cardTitle : undefined,
+				type === 'cardText' ? styles.cardText : undefined,
+				type === 'small' ? styles.small : undefined,
+				style,
+			]}
+			{...rest}
+        />
+    );
 }
 
 const styles = StyleSheet.create({
@@ -38,23 +38,23 @@ const styles = StyleSheet.create({
     fontSize: 16,
     lineHeight: 24,
   },
-  defaultSemiBold: {
-    fontSize: 16,
+  cardTitle: {
+    fontSize: 14,
     lineHeight: 24,
     fontWeight: '600',
   },
-  title: {
-    fontSize: 32,
+  heading: {
+    fontSize: 20,
     fontWeight: 'bold',
     lineHeight: 32,
   },
-  subtitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  cardText: {
+    fontSize: 12,
+    lineHeight: 15
   },
-  link: {
-    lineHeight: 30,
-    fontSize: 16,
-    color: '#0a7ea4',
+  small: {
+    lineHeight: 20,
+    fontSize: 10,
+    color: '#908f8f',
   },
 });
