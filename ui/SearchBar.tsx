2692aae34b0cec8b569c176b635180183dd532c8
Baffuoh Asare-Bediako <104689768+baffuohasareb@users.noreply.github.com>
2024-09-15 22:46:34 +0000

    Added SearchBar component

--- ui/SearchBar.tsx
+++ ui/SearchBar.tsx
@@ -1,15 +1,19 @@
 import { StyleSheet, Text, TextInput, View } from "react-native"
 import React, { FC } from "react";
+import Search from "../assets/icons/search.svg"
+import { useThemeColor } from "@/hooks/useThemeColor";
 
 type SearchBarProps = {
     placeholder: string;
 }
 
 const SearchBar: FC<SearchBarProps> = ({ placeholder }) => {
+    const color = useThemeColor({}, "tint");
     return (
-        <View>
+        <View style={[styles.container, { borderColor: color}]}>
             <TextInput
-                placeholder="Search notes, folders, and tags"
+                placeholder={placeholder}
+                placeholderTextColor={color}
             />
         </View>
     )
@@ -17,4 +21,13 @@ const SearchBar: FC<SearchBarProps> = ({ placeholder }) => {
 
 export default SearchBar
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        borderWidth: 2,
+        borderRadius: 20,
+        paddingHorizontal: 15,
+        paddingVertical: 7
+    }
+})
