6092c0597b41a76f23d1dbf04d60d3764f22edaa
Baffuoh Asare-Bediako <104689768+baffuohasareb@users.noreply.github.com>
2024-10-05 22:10:56 +0000

    Added AddNoteButton and search icon

--- ui/SearchBar.tsx
+++ ui/SearchBar.tsx
@@ -1,20 +1,29 @@
 import { StyleSheet, Text, TextInput, View } from "react-native"
 import React, { FC } from "react";
-import Search from "../assets/icons/search.svg"
+import Search from "../components/icons/Search"
 import { useThemeColor } from "@/hooks/useThemeColor";
+import AddNoteButton from "./AddNoteButton";
 
 type SearchBarProps = {
     placeholder: string;
 }
 
 const SearchBar: FC<SearchBarProps> = ({ placeholder }) => {
-    const color = useThemeColor({}, "tint");
+    const text = useThemeColor({}, "text");
+    const tint = useThemeColor({}, "tint")
     return (
-        <View style={[styles.container, { borderColor: color}]}>
-            <TextInput
-                placeholder={placeholder}
-                placeholderTextColor={color}
-            />
+        <View style={styles.container}>
+            <View style={[styles.searchBar, { borderColor: tint}]}>
+                <TextInput
+                    placeholder={placeholder}
+                    placeholderTextColor={text}
+                    style={{ fontWeight: "200", color: text}}                
+                />
+
+                <Search  size={25} color={tint} />
+            </View>
+
+            <AddNoteButton/>
         </View>
     )
 }
@@ -24,10 +33,18 @@ export default SearchBar
 const styles = StyleSheet.create({
     container: {
         flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        gap: 10
+    },
+    searchBar: {
+        flexDirection: "row",
         justifyContent: "space-between",
         borderWidth: 2,
         borderRadius: 20,
         paddingHorizontal: 15,
-        paddingVertical: 7
+        paddingVertical: 7,
+        marginVertical: 20,
+        flex: 1
     }
 })
