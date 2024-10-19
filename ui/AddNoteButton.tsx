7f45aa84ee422d90aa85bfa5a63158ae4093d97e
Baffuoh Asare-Bediako <104689768+baffuohasareb@users.noreply.github.com>
2024-10-19 21:08:08 +0000

    Implemented navigation to edit note screen

--- ui/AddNoteButton.tsx
+++ ui/AddNoteButton.tsx
@@ -2,11 +2,18 @@ import { Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import AddNote from '@/components/icons/AddNote'
 import { useThemeColor } from '@/hooks/useThemeColor'
+import { useRouter } from 'expo-router'
 
 const AddNoteButton = () => {
     const tint = useThemeColor({}, "tint");
+    const router = useRouter();
+
     return (
-        <Pressable style={[styles.container, { backgroundColor: tint }]}>
+        <Pressable 
+            onPress={() => {
+                router.push("/editNote");
+            }}
+            style={[styles.container, { backgroundColor: tint }]}>
             <AddNote size={25} color={"#ffffff"} />
         </Pressable>
     )
